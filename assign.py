8d27af6b031c2b929bacb8b2264ed9e3f8b5fd07
AMITH1099 <132582004+AMITH1099@users.noreply.github.com>
2023-10-02 00:03:01 +0530

    Add files via upload

---
+++ assign.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('terms.csv')
+
+print(df)
+pname = df['program_code'].head(8)
+c_hr = df['cumulative_credit_hours'].head(8)
+fig = plt.figure(figsize =(10, 7))
+plt.bar(pname[0:10], c_hr[0:10])
+# Show Plot
+plt.show()
